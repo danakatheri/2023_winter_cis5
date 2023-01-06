cc7209b83f6d816e5867464d8a95c725e29031f1
danakatheri <danakrodriguez@gmail.com>
2023-01-06 14:51:30 -0800

    class?

---
+++ class/helloworld/main.cpp
@@ -0,0 +1,45 @@
+
+
+/* 
+ * File:   main.cpp
+ * Author: danak
+ * Created on January 6, 2023, 2:08 PM
+ * Purpose: Hello World
+ *      First Program all programmers write
+ *      when testing their IDE for the first
+ *      time.
+ *      C: Programming Language by
+ *      Dennis Ritchie and Brian Kernighan
+ * 
+ */
+//System Lib
+
+#include <iostream> //Input Output Library
+
+using namespace std;
+
+//User LIBRARIES
+ 
+ //Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+     //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/process the Inputs-> Outputs
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    //Clean up memory and files
+    
+    //Exit the program
+    return 0;
+}
+
