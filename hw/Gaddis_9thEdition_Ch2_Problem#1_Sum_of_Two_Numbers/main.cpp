51f8489797e60a58c05eef73d54cea7664dfe5f5
danakatheri <danakrodriguez@gmail.com>
2023-01-09 14:00:32 -0800

    updated

---
+++ hw/Gaddis_9thEdition_Ch2_Problem#1_Sum_of_Two_Numbers/main.cpp
@@ -0,0 +1,39 @@
+
+/* 
+ * File:   main.cpp
+ * Author: danak
+ * Created on January 6, 2023, 2:08 PM
+ * Purpose: Template to be used for all future hw,labs, exams, projects
+ * 
+ */
+//System Lib
+
+#include <iostream> //Input Output Library
+
+using namespace std;
+
+//User LIBRARIES
+ 
+ //Global Constants not Variables
+
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+     //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/process the Inputs-> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the program
+    return 0;
+}
