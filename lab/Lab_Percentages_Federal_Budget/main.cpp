5c8399723cab0fe4a4719896238008d38700c54e
danakatheri <danakrodriguez@gmail.com>
2023-01-11 19:18:32 -0800

    update 1/11 7:18pm

---
+++ lab/Lab_Percentages_Federal_Budget/main.cpp
@@ -0,0 +1,43 @@
+
+/* 
+ * File:   main.cpp
+ * Author: danak
+ * Created on January 11, 2023, 6:02 PM
+ * Purpose: Lab Assignment Percentages 
+ * 
+ */
+//System Lib
+
+#include <iostream> //Input Output Library
+
+using namespace std;
+
+//User LIBRARIES
+ 
+ //Global Constants not Variables
+
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    float milBdgt,fedBdgt,mlPrcnt;
+    
+    milBdgt=7.77e11f;    //Military Budget = 777 Billion   
+    fedBdgt=6.272e12f;    //Federal Budget  = 6.272 Trillion
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/process the Inputs-> Outputs
+    
+    //Display Inputs/Outputs
+    
+    //Clean up memory and files
+    
+    //Exit the program
+    return 0;
+}
+
